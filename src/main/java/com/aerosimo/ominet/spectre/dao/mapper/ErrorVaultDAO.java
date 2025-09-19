330160f049075ff9171b5d7624023b0b414be727
Elijah Omisore <elijah@omisore.co.uk>
2025-09-19 19:23:54 +0100

    [AER19092025] - Incrementally adding all other classes

---
+++ src/main/java/com/aerosimo/ominet/spectre/dao/mapper/ErrorVaultDAO.java
@@ -0,0 +1,110 @@
+/******************************************************************************
+ * This piece of work is to enhance spectre project functionality.            *
+ *                                                                            *
+ * Author:    eomisore                                                        *
+ * File:      ErrorVaultDAO.java                                              *
+ * Created:   19/09/2025, 19:20                                               *
+ * Modified:  19/09/2025, 19:20                                               *
+ *                                                                            *
+ * Copyright (c)  2025.  Aerosimo Ltd                                         *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,            *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES            *
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                   *
+ * NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT                 *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,               *
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING               *
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE                 *
+ * OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.aerosimo.ominet.spectre.dao.mapper;
+
+import com.aerosimo.ominet.spectre.com.mail.ErrorMail;
+import com.aerosimo.ominet.spectre.core.config.Connect;
+import com.aerosimo.ominet.spectre.dao.impl.ErrorResponseDTO;
+import oracle.jdbc.OracleTypes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ErrorVaultDAO {
+
+    private static final Logger log;
+
+    static {
+        log = LogManager.getLogger(ErrorVaultDAO.class.getName());
+    }
+
+    static String response;
+    static String sql;
+    static List<ErrorResponseDTO> errorList;
+    static Connection con;
+    static CallableStatement stmt;
+
+    public static String storeError(String faultCode, String faultMessage, String faultService) {
+        log.info("Preparing to store new error into error vault");
+        try {
+            sql = "{call error_vault_pkg.store_error(?,?,?,?)}";
+            con = Connect.dbase();
+            stmt = con.prepareCall(sql);
+            stmt.setString(1, faultCode);
+            stmt.setString(2, faultMessage);
+            stmt.setString(3, faultService);
+            stmt.registerOutParameter(4, Types.VARCHAR);
+            stmt.execute();
+            response = stmt.getString(4);
+            ErrorMail.mail(stmt.getString(4), faultCode, faultMessage);
+            log.info("Successfully store new error into error vault");
+        } catch (SQLException err) {
+            response = "ErrorVaultDAO (storeError) attempt failed";
+            log.error("Unknown error occurred in ErrorVaultDAO (storeError) with the following - {}", ErrorVaultDAO.class.getName(), err);
+        }
+        return response;
+    }
+
+    public static List<ErrorResponseDTO> getTopErrors(int records){
+        errorList = new ArrayList<>();
+        sql = "{call error_vault_pkg.get_errors(?,?)}";
+        ResultSet rs;
+        try {
+            Connection con;
+            con = Connect.dbase();
+            stmt = con.prepareCall(sql);
+            stmt.setInt(1, records);
+            stmt.registerOutParameter(2, OracleTypes.CURSOR);
+            stmt.execute();
+            rs = (ResultSet) stmt.getObject(2);
+            while (rs.next()) errorList.add(new ErrorResponseDTO(rs.getInt(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6)));
+        } catch (Exception err) {
+            log.error("Database adaptor error occurred with the following - {}", ErrorVaultDAO.class.getName(), err);
+        } finally {
+            try {
+                assert stmt != null;
+                stmt.close();
+            } catch (SQLException err) {
+                log.error("Database adaptor error occurred with the following - {}", ErrorVaultDAO.class.getName(), err);
+            }
+        }
+        return errorList;
+    }
+}
---
+++ src/main/java/com/aerosimo/ominet/spectre/service/api/SpectreSOAP.java
@@ -0,0 +1,86 @@
+/******************************************************************************
+ * This piece of work is to enhance spectre project functionality.            *
+ *                                                                            *
+ * Author:    eomisore                                                        *
+ * File:      SpectreSOAP.java                                                *
+ * Created:   19/09/2025, 19:22                                               *
+ * Modified:  19/09/2025, 19:22                                               *
+ *                                                                            *
+ * Copyright (c)  2025.  Aerosimo Ltd                                         *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included    *
+ * in all copies or substantial portions of the Software.                     *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,            *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES            *
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                   *
+ * NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT                 *
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,               *
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING               *
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE                 *
+ * OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+ *                                                                            *
+ ******************************************************************************/
+
+package com.aerosimo.ominet.spectre.service.api;
+
+import com.aerosimo.ominet.spectre.dao.impl.ErrorResponseDTO;
+import com.aerosimo.ominet.spectre.dao.mapper.ErrorVaultDAO;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebResult;
+import jakarta.jws.WebService;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.ws.BindingType;
+import jakarta.xml.ws.soap.SOAPBinding;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+@WebService(name = "spectre", serviceName = "spectre",
+        portName = "spectreport", targetNamespace = "https://aerosimo.com/api/ws/spectre")
+@BindingType(SOAPBinding.SOAP12HTTP_BINDING)
+public class SpectreSOAP {
+
+    private static final Logger log;
+
+    static {
+        log = LogManager.getLogger(SpectreSOAP.class.getName());
+    }
+    static String Response;
+
+    @WebMethod(operationName = "recordError")
+    @WebResult(name = "recordError", partName = "recordErrorResponse")
+    public String recordError(@XmlElement(required = true) @WebParam(name = "faultcode", partName = "recordErrorRequest") String faultcode,
+                              @XmlElement(required = true) @WebParam(name = "faultmessage", partName = "recordErrorRequest") String faultmessage,
+                              @XmlElement(required = true) @WebParam(name = "faultservicename", partName = "recordErrorRequest") String faultservicename){
+
+        if (faultcode.isEmpty()) {
+            Response = "Schema Validation failed because Fault Code is a required field";
+            log.error(Response);
+        } else if (faultmessage.isEmpty()) {
+            Response = "Schema Validation failed because Fault Message is a required field";
+            log.error(Response);
+        } else if (faultservicename.isEmpty()) {
+            Response = "Schema Validation failed because Service Name is a required field";
+            log.error(Response);
+        }
+        Response = ErrorVaultDAO.storeError(faultcode, faultmessage, faultservicename);
+        log.info(Response);
+        return Response;
+    }
+
+    @WebMethod(operationName = "getTopErrors")
+    @WebResult(name = "getTopErrors", partName = "getTopErrorsResponse")
+    public List<ErrorResponseDTO> getTopErrors(@XmlElement(required = true)@WebParam(name = "records", partName = "getTopErrorsRequest") int records) {
+        return ErrorVaultDAO.getTopErrors(records);
+    }
+}
